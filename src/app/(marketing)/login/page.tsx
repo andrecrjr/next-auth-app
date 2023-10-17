6e56795b7ddfe0b94d1782428295407a1a559b1c
André Carlos Jr <7965520+andrecrjr@users.noreply.github.com>
2023-10-16 22:39:41 -0300

    - pagina de signup

---
+++ src/app/(marketing)/login/page.tsx
@@ -0,0 +1,70 @@
+import { Metadata } from "next"
+import Image from "next/image"
+import Link from "next/link"
+
+import { cn } from "@/lib/utils"
+import { buttonVariants } from "@/components/ui/button"
+import { UserLoginForm } from "@/components/user-login-auth"
+
+export const metadata: Metadata = {
+  title: "Eroshi",
+  description: "Eroshi Autenticação com Shadcn-ui, Next Auth, MongoDB",
+}
+
+export default function AuthenticationPage() {
+  return (
+    <>
+      <div className="flex container relative h-screen flex-col 
+      items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+        <Link
+          href="/examples/authentication"
+          className={cn(
+            buttonVariants({ variant: "ghost" }),
+            "absolute right-4 top-4 md:right-8 md:top-8"
+          )}
+        >
+          Login
+        </Link>
+        <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
+          <div className="absolute inset-0 bg-zinc-900" />
+          <div className="relative z-20 flex items-center text-lg font-medium">
+            Eroshi INC.
+          </div>
+          <div className="relative z-20 mt-auto">
+
+          </div>
+        </div>
+        <div className="items-center self-center lg:p-8">
+          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+            <div className="flex flex-col space-y-2 text-center">
+              <h1 className="text-2xl font-semibold tracking-tight">
+                Crie uma conta
+              </h1>
+              <p className="text-sm text-muted-foreground">
+                Preencha as informações necessários para criar sua conta
+              </p>
+            </div>
+            <UserLoginForm />
+            <p className="px-8 text-center text-sm text-muted-foreground">
+              Clique e aceite nossos{" "}
+              <Link
+                href="/terms"
+                className="underline underline-offset-4 hover:text-primary"
+              >
+                Termos de Serviço
+              </Link>{" "}
+              e{" "}
+              <Link
+                href="/privacy"
+                className="underline underline-offset-4 hover:text-primary"
+              >
+                Politica de Privacidade
+              </Link>
+              .
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
---
+++ src/components/user-login-auth.tsx
@@ -0,0 +1,87 @@
+'use client'
+import { cn } from "@/lib/utils";
+import React, { useState } from "react";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Icons } from "./ui/icons";
+import { signIn } from "next-auth/react";
+
+interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement>{
+
+}
+
+interface IUser{
+    email: string;
+    password:string;
+}
+
+export function UserLoginForm({className, ...props}:UserAuthFormProps){
+    const [data, setData] = useState<IUser>({
+        email:"",
+        password:""
+    })
+
+    const [isLoading, setisLoading] = useState(false);
+    
+
+    async function _onSubmit(e:React.FormEvent){
+        e.preventDefault()
+        setisLoading(true)
+
+        const res = await signIn<"credentials">("credentials",{...data, redirect:false})
+    
+ 
+        setData({email:"", password:""})
+        setisLoading(false)
+    }
+
+    const handleChange = async (e:React.ChangeEvent<HTMLInputElement>) =>{
+        setData(prevData=>{
+            return {...prevData, [e.target.name]:e.target.value}
+        })
+    }
+
+    return (
+        <section className={cn("grid gap-6", className)} {...props}>
+            
+            <form onSubmit={_onSubmit}>
+                <div className="grid gap-2">
+                    <fieldset className="grid gap-1">
+                        <Label className="sr-only" htmlFor="email">E-mail</Label>
+                        <Input type="email" 
+                            id="email" 
+                            autoCapitalize="none"
+                            autoCorrect="off"
+                            autoComplete="email"
+                            disabled={isLoading}
+                            name="email"
+                            value={data.email}
+                            placeholder="E-mail"
+                            onChange={handleChange}
+                        />
+                        </fieldset>
+                    <fieldset className="grid gap-1">
+                    <Label className="sr-only" htmlFor="password">Password</Label>
+                    <Input type="password" 
+                        id="password" 
+                        autoCapitalize="none"
+                        autoCorrect="off"
+                        autoComplete="password"
+                        disabled={isLoading}
+                        name="password"
+                        value={data.password}
+                        placeholder="Senha"
+                        onChange={handleChange}
+                    />
+                    </fieldset>
+
+                </div>
+                
+                <Button type="submit" className="w-full mt-4">{isLoading && <Icons.spinner className="mr-2 h-4 animate-spin"/>} Entrar</Button>
+            </form>
+
+        </section>
+    )
+
+}
